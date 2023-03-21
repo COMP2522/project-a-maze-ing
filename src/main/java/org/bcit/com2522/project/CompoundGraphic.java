70ed83ce2702fb22ff9fd5f7036bde9b4c079a21
AlexSichitiu <asichitiu@gmail.com>
2023-03-20 17:05:59 -0700

    Commit in order to pull from main.

--- src/main/java/org/bcit/com2522/project/CompoundGraphic.java
+++ src/main/java/org/bcit/com2522/project/CompoundGraphic.java
@@ -7,14 +7,26 @@ import java.util.ArrayList;
 public class CompoundGraphic implements Graphic{
   ArrayList<Graphic> graphics = new ArrayList<Graphic>();
 
+  /**
+   * Add a Graphic to this CompoundGraphic.
+   * @param g An object that implements Graphic
+   */
   public void add(Graphic g){
     graphics.add(g);
   }
 
+  /**
+   * Remove a Graphic from this CompoundGraphic.
+   * @param g An object that implements Graphic
+   */
   public void remove(Graphic g){
     graphics.remove(g);
   }
 
+  /**
+   * Return the list of graphics in this CompoundGraphic.
+   * @return the list of Graphic objects
+   */
   public ArrayList<Graphic> get(){
     return graphics;
   }
