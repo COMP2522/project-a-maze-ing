12869f8bc5ea84a81e16f7850d78129a2d03a685
Matthew Siggs <mcsiggs@shaw.ca>
2023-03-23 13:21:39 -0700

    added wall class, implemented collidable interface in tile

--- src/main/java/org/bcit/com2522/project/labyrinth/Tiles/Tile.java
+++ src/main/java/org/bcit/com2522/project/labyrinth/Tiles/Tile.java
@@ -1,8 +1,23 @@
 package org.bcit.com2522.project.labyrinth.Tiles;
 
-public abstract class Tile {
+import org.bcit.com2522.project.interfaces.Collidable;
+import processing.core.PVector;
 
+public abstract class Tile implements Collidable {
+
+  // The side length of a tile
   public static final int TILE_SIZE = 250; // placeholder value, adjust as needed
+
+  private PVector position;
+
+  public Tile(PVector pos) {
+    position = pos;
+  }
+
+  @Override
+  public boolean collision(Collidable a, Collidable b) {
+    return false;
+  }
 
 
 
--- src/main/java/org/bcit/com2522/project/labyrinth/Tiles/Wall.java
+++ src/main/java/org/bcit/com2522/project/labyrinth/Tiles/Wall.java
@@ -1,2 +1,16 @@
-package org.bcit.com2522.project.labyrinth.Tiles;public class Wall {
+package org.bcit.com2522.project.labyrinth.Tiles;
+
+import org.bcit.com2522.project.interfaces.Collidable;
+import processing.core.PVector;
+
+public class Wall extends Tile {
+
+  public Wall(PVector pos) {
+    super(pos);
+  }
+
+  @Override
+  public boolean collision(Collidable a, Collidable b) {
+    return false;
+  }
 }
