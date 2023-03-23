fef9861d5aa024bff6bc40cbc709b15da1fcf484
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-03-22 22:05:57 -0700

    adding Drawable interface

---
+++ src/main/java/org/bcit/com2522/project/Trap.java
@@ -0,0 +1,16 @@
+package org.bcit.com2522.project;
+
+import processing.core.PVector;
+
+import java.awt.*;
+
+public class Trap extends Sprite{
+    public Trap(PVector position, PVector direction, float size, float speed, Color color, Window window) {
+        super(position, direction, size, speed, color, window);
+    }
+
+    @Override
+    public boolean collision(Sprite s) {
+        return false;
+    }
+}
---
+++ src/main/java/org/bcit/com2522/project/interfaces/Drawable.java
@@ -0,0 +1,4 @@
+package org.bcit.com2522.project.interfaces;
+
+public interface Drawable {
+}
