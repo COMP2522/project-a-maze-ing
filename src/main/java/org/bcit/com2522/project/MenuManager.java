b8d94d5f0453869b9aea0c7725942f11c2b8f64e
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-04-09 19:28:02 -0700

    menu manager javadocs

--- src/main/java/org/bcit/com2522/project/MenuManager.java
+++ src/main/java/org/bcit/com2522/project/MenuManager.java
@@ -3,32 +3,62 @@ package org.bcit.com2522.project;
 import com.mongodb.client.FindIterable;
 import org.bcit.com2522.project.Database.Database;
 import org.bson.Document;
-
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * The MenuManager class is responsible for managing menus in the game.
+ * It is a singleton class that maintains instances of the main menu and pause menu,
+ * as well as any other menus added to the game.
+ * The MenuManager can load and draw menus and their associated buttons,
+ * and return lists of saved maze buttons.
+ * @author Matthew Siggs
+ * @author Laurie Solkoski
+ * @version 1.0
+ */
 public class MenuManager {
 
+  /*
+  The singleton instance of the MenuManager.
+   */
   private static MenuManager instance;
 
-
-
+  /*
+  The main menu for the game.
+   */
   private MainMenu mainMenu;
+
+  /*
+  The pause menu for the game.
+   */
   private PauseMenu pauseMenu;
+
+  /*
+  The list of saved maze buttons.
+   */
   private ArrayList<Button> savedMazeButtons = new ArrayList<>();
 
-
+  /*
+  The list of menus managed by the MenuManager.
+   */
   private ArrayList<Menu> menus;
 
+  /*
+  * Private constructor for the MenuManager singleton.
+  * Initializes the main menu, pause menu, and menus list.
+   */
   private MenuManager(){
     mainMenu = new MainMenu();
     pauseMenu = new PauseMenu();
     menus = new ArrayList<>();
-
     menus.add(mainMenu);
     menus.add(pauseMenu);
   }
 
+  /**
+   * Returns the singleton instance of the MenuManager.
+   * @return the singleton instance of the MenuManager
+   */
   public static MenuManager getInstance() {
     if (instance == null) {
       instance = new MenuManager();
@@ -44,10 +74,18 @@ public class MenuManager {
     return savedMazeButtons;
   }
 
+  /**
+   * Returns the list of menus managed by the MenuManager.
+   * @return the list of menus managed by the MenuManager
+   */
   public ArrayList<Menu> getMenus() {
     return menus;
   }
 
+  /**
+   * Returns the main menu.
+   * @return the main menu
+   */
   public MainMenu getMainMenu() {
     return mainMenu;
   }
@@ -63,6 +101,10 @@ public class MenuManager {
     pauseMenu.draw();
   }
 
+  /**
+   * Loads saved maze buttons and draws them to the screen.
+   * The saved maze buttons are loaded from the database using the Database class.
+   */
   public void loadSavedMazeButtons() {
     int buttonIndex = 0;
     FindIterable<Document> savedMazes = Database.getInstance().loadAll();
