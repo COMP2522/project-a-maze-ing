aa9fccd17a9e17288ef5761b7cd01156a3299674
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-04-09 18:25:54 -0700

    javadocs

--- src/main/java/org/bcit/com2522/project/Database/Database.java
+++ src/main/java/org/bcit/com2522/project/Database/Database.java
@@ -10,13 +10,37 @@ import java.util.Arrays;
 
 import static com.mongodb.client.model.Filters.eq;
 
-
+/**
+ * The Database class represents a MongoDB database used to store labyrinths.
+ * This class is a singleton, meaning that only one instance of the class is allowed to exist at any given time.
+ * The Database instance is responsible for initializing the MongoDB client, connecting to the "test" database,
+ * and providing methods for saving and loading labyrinths to/from the database.
+ * @author Matthew Siggs
+ * @author Laurie Solkoski
+ * @version 1.0
+ */
 public class Database {
 
+  /**
+   * Private static field for the singleton instance of the Database class.
+   */
   private static Database instance;
-  private String labyrinthName;
+
+  /**
+   * Private field for the MongoDB database associated with the Database instance.
+   */
   private MongoDatabase database;
+
+  /**
+   * Private field for the MongoDB client associated with the Database instance.
+   */
   private MongoClient client;
+
+  /**
+   * Private constructor for the Database class.
+   * Initializes the MongoDB client and connects to the "test" database.
+   * This constructor should only be called once, when the singleton instance is created.
+   */
   private Database() {
     ConnectionString connectionString = new ConnectionString("mongodb+srv://maze:1234@a-maze-ing.mu66nzj.mongodb.net/?retryWrites=true&w=majority");
     MongoClientSettings settings = MongoClientSettings.builder()
@@ -30,6 +54,10 @@ public class Database {
 
   }
 
+  /**
+   * Returns the singleton instance of the Database class.
+   * @return the singleton instance of the Database class
+   */
   public static Database getInstance(){
     if(instance == null) {
       instance = new Database();
@@ -68,6 +96,10 @@ public class Database {
     LabyrinthManager.getInstance().loadLabyrinth(target);
   }
 
+  /**
+   * Returns all documents in the "labyrinths" collection.
+   * @return a {@code FindIterable<Document>} containing all documents in the "labyrinths" collection
+   */
   public FindIterable<Document> loadAll() {
     FindIterable<Document> labyrinths = database.getCollection("labyrinths").find();
     return labyrinths;
