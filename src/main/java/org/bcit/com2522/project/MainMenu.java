50dacb5d3787cc271ad534c71eba41f646b69d2b
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-04-09 19:19:03 -0700

    Main menu javadocs

--- src/main/java/org/bcit/com2522/project/MainMenu.java
+++ src/main/java/org/bcit/com2522/project/MainMenu.java
@@ -2,10 +2,25 @@ package org.bcit.com2522.project;
 
 import java.awt.*;
 
-
+/**
+ * The MainMenu class represents the main menu of the game.
+ * It extends the Menu class and includes buttons for playing the game and loading all saved mazes.
+ * The MainMenu class uses Button objects to create and manage its buttons.
+ * @author Laurie Solkoski
+ * @author Alex Sichitiu
+ * @author Matthew Siggs
+ * @version 1.0
+ */
 public class MainMenu extends Menu {
 
+  /*
+  Names of the buttons.
+   */
   Button play, loadAll;
+
+  /**
+   * Constructs a new MainMenu with "Play" and "Load All Saved Mazes" buttons.
+   */
   public MainMenu() {
     play = new Button("Play", 100, 100, 200, 100, Color.BLUE, this);
     play.config(() -> GameManager.getInstance().window.initializeObjects());
@@ -15,8 +30,9 @@ public class MainMenu extends Menu {
     loadAll.config(() -> GameManager.getInstance().setState(GameState.LOAD_ALL));
   }
 
-
-  // Method to return to the main menu from the saved maze buttons screen
+  /*
+  Returns to the main menu from the saved maze buttons screen.
+   */
   public void returnToMainMenu() {
     GameManager.getInstance().setState(GameState.MENU);
   }
