571da0be4e04c9e438c037efbfd67c066f51387f
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-04-09 19:41:19 -0700

    TextBox javadocs

--- src/main/java/org/bcit/com2522/project/TextBox.java
+++ src/main/java/org/bcit/com2522/project/TextBox.java
@@ -5,12 +5,41 @@ import processing.core.PApplet;
 import processing.core.PVector;
 import processing.event.MouseEvent;
 
+/**
+ * This class represents a text box in the user interface.
+ * Allows users to enter text and save a labyrinth with a given name.
+ * @author Laurie Solkoski
+ * @version 1.0
+ */
 public class TextBox {
+
+    /*
+    The PApplet instance associated with the TextBox.
+     */
     PApplet parent;
+
+    /*
+    The position of the top-left corner of the TextBox.
+     */
     PVector position;
+
+    /*
+    Width and height of text box.
+     */
     int width, height;
+
+    /*
+    The text currently contained within the TextBox.
+     */
     String text = "";
 
+    /**
+     * Creates a new TextBox with the specified position, width, and height.
+     * @param parent the parent PApplet
+     * @param position the position of the TextBox as a PVector
+     * @param width the width of the TextBox
+     * @param height the height of the TextBox
+     */
     TextBox(PApplet parent, PVector position, int width, int height) {
         this.parent = parent;
         this.position = position;
@@ -18,6 +47,9 @@ public class TextBox {
         this.height = height;
     }
 
+    /*
+    Draws the TextBox on the screen.
+     */
     void draw() {
         parent.fill(255);
         parent.rect(position.x, position.y, width, height);
@@ -25,6 +57,12 @@ public class TextBox {
         parent.text(text, position.x + 5, position.y + height - 5);
     }
 
+    /**
+     * Handles a mouse click event on the TextBox.
+     * If the saveButton is clicked and the text in the TextBox is not empty,
+     * saves the current labyrinth with the given name and closes the TextBox.
+     * @param m the MouseEvent to handle
+     */
     public void mouseClicked(MouseEvent m) {
         if (parent instanceof Window) {
             Window win = (Window) parent;
@@ -37,20 +75,37 @@ public class TextBox {
         }
     }
 
+    /**
+     * Checks if the specified coordinates are within the bounds of the TextBox.
+     * @param x the x-coordinate to check
+     * @param y the y-coordinate to check
+     * @return true if the coordinates are within the bounds of the TextBox, false otherwise
+     */
     boolean contains(int x, int y) {
         return x >= position.x && x <= position.x + width && y >= position.y && y <= position.y + height;
     }
 
+    /**
+     * Adds the specified character to the text in the TextBox.
+     * @param c the character to add
+     */
     void addChar(char c) {
         text += c;
     }
 
+    /*
+    Removes the last character from the text in the TextBox.
+     */
     void removeChar() {
         if (text.length() > 0) {
             text = text.substring(0, text.length() - 1);
         }
     }
 
+    /**
+     * Gets the text currently in the TextBox.
+     * @return the text in the TextBox
+     */
     String getText() {
         return text;
     }
